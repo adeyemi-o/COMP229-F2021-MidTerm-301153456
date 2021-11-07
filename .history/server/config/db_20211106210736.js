d960304ef5d45257a6da7b6244ccf41d7f7999cd
iamdebolax <aoyinlo4@my.centennialcollege.ca>
2021-11-06 23:15:46 -0400

    midterm

---
+++ .history/server/config/db_20211106210736.js
@@ -0,0 +1,5 @@
+module.exports = {
+    //local MongoDB deployment ->
+    //"URI": "mongodb://localhost/books229"
+    "URI": "mongodb+srv://adeyemi:xjoXkv17W2p0n2dy@mongodbserver.bqhte.mongodb.net/book_store?retryWrites=true&w=majority"
+};
